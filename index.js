12bdf6eb332a1f7832f378c9dcdec642e89f6497
Tyowk <tyowk.personal@gmail.com>
2024-10-19 12:18:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const fetchUserData = async (req, res, next) => {
     next();
 };
 
-app.get('/guilds', async (_, res) => {
+app.get('/api/guilds', async (_, res) => {
     try {
         const [guildResponse, channelsResponse] = await Promise.all([
             axios.get(`https://discord.com/api/v10/guilds/${process.env.GUILD_ID}?with_counts=true`, {
@@ -55,7 +55,7 @@ app.get('/guilds', async (_, res) => {
     }
 });
 
-app.post('/submit', fetchUserData, async (req, res) => {
+app.post('/submit/requirements', fetchUserData, async (req, res) => {
     try {
         const user = req.user;
         if (!user) return res.status(401).json({ message: 'Unauthorized', code: 401 });
@@ -95,7 +95,7 @@ app.all('/auth/discord', (req, res) => {
     res.redirect(process.env.AUTH_URL);
 });
 
-app.get('/profile', fetchUserData, async (req, res) => {
+app.get('/api/profile', fetchUserData, async (req, res) => {
     const user = req.user;
     if (!user) return res.status(401).json({ message: 'Unauthorized', code: 401 });
 
